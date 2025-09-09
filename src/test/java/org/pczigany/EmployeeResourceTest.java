cae201b285323b9440e4808ca9b7c793ad836a0f
Péter Czigány <p.czigany@gmail.com>
2025-09-09 07:38:26 +0200

    fix first acceptance test case

--- src/test/java/org/pczigany/EmployeeResourceTest.java
+++ src/test/java/org/pczigany/EmployeeResourceTest.java
@@ -1,27 +1,37 @@
 package org.pczigany;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class EmployeeResourceTest {
+    /**
+     * User Acceptance Test of the {@link EmployeeResource#names() names} endpoint
+     */
     @Test
     void testNamesEndpoint() {
-        given()
-                .when().get("/rest/employees")
-                .then()
-                .statusCode(200)
-                .body(is("[ " +
-                        "\"Dale Miller\"," +
-                        "\"George Smith\"," +
-                        "\"James Doyle\"," +
-                        "\"Joanne Olsen\"," +
-                        "\"Michael Smith\"," +
-                        "\"Peter Goeking\"," +
-                        "\"Samuel Palmisano\"" +
-                        " ]"));
+        final Response response = RestAssured.get("/rest/employees");
+        MatcherAssert.assertThat(
+                "Response status is not 200 OK",
+                response.getStatusCode(),
+                Matchers.is(Matchers.equalTo(200))
+        );
+        MatcherAssert.assertThat(
+                "Response body is not correct",
+                response.getBody().asString(),
+                Matchers.is(Matchers.equalTo(
+                        "[\"Dale Miller\","
+                                + "\"George Smith\","
+                                + "\"James Doyle\","
+                                + "\"Joanne Olsen\","
+                                + "\"Michael Smith\","
+                                + "\"Peter Goeking\","
+                                + "\"Samuel Palmisano\"]"
+                ))
+        );
     }
 }
