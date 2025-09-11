f6bc1aa69c36530a7b0c8717c16a785dca605f62
Péter Czigány <p.czigany@gmail.com>
2025-09-11 02:28:49 +0200

    refactor test case

--- src/test/java/org/pczigany/DefaultNameTest.java
+++ src/test/java/org/pczigany/DefaultNameTest.java
@@ -1,6 +1,5 @@
 package org.pczigany;
 
-import com.jcabi.xml.XML;
 import com.jcabi.xml.XMLDocument;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -9,17 +8,16 @@ import org.junit.jupiter.api.Test;
 class DefaultNameTest {
     @Test
     void text() {
-        final XML employeeNode = new XMLDocument(
-                """
-                        <employee>
-                            <name>John Doe</name>
-                            <department>it</department>
-                        </employee>
-                        """
-        );
         MatcherAssert.assertThat(
                 "the returned string is not correct",
-                new DefaultName(employeeNode).text(),
+                new DefaultName(new XMLDocument(
+                        """
+                                <employee>
+                                    <name>John Doe</name>
+                                    <department>it</department>
+                                </employee>
+                                """
+                )).text(),
                 Matchers.is(Matchers.equalTo("John Doe"))
         );
     }
