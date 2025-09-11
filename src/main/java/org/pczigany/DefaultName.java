63b52a0423d9826b4555e75e5ab9cda4e8477770
Péter Czigány <p.czigany@gmail.com>
2025-09-11 02:13:16 +0200

    complete class

--- src/main/java/org/pczigany/DefaultName.java
+++ src/main/java/org/pczigany/DefaultName.java
@@ -2,6 +2,8 @@ package org.pczigany;
 
 import com.jcabi.xml.XML;
 
+import java.util.Objects;
+
 public class DefaultName implements Name {
     private final XML employeeNode;
 
@@ -12,5 +14,19 @@ public class DefaultName implements Name {
     @Override
     public String text() {
         return this.employeeNode.xpath("//name/text()").get(0);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        final DefaultName that = (DefaultName) o;
+        return Objects.equals(this.employeeNode, that.employeeNode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.employeeNode);
     }
 }
--- src/main/java/org/pczigany/Name.java
+++ src/main/java/org/pczigany/Name.java
@@ -1,5 +1,11 @@
 package org.pczigany;
 
+/**
+ * The name of an employee
+ */
 public interface Name {
+    /**
+     * @return the name as a String, e.g. Johann Schmidt
+     */
     String text();
 }
