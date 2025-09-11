edc8a78cfb41dc207a2a7532ccf45a762e411ba9
Péter Czigány <p.czigany@gmail.com>
2025-09-11 02:55:40 +0200

    JavaDoc

--- src/main/java/org/pczigany/Employee.java
+++ src/main/java/org/pczigany/Employee.java
@@ -2,7 +2,14 @@ package org.pczigany;
 
 import java.util.List;
 
+/**
+ * An employee, working inside some departments
+ */
 public interface Employee {
+    /**
+     * @return the name as a String, e.g. Johann Schmidt
+     */
     String getName();
+
     List<String> getDepartments();
 }
