d298f97c369505f02a23aac99ea2f5e7804b48d7
Czigány Péter <czigany.peter@telekom.hu>
2025-09-15 17:49:38 +0200

    debug detailed equality

--- src/test/java/org/pczigany/PathAsEmployeeListTest.java
+++ src/test/java/org/pczigany/PathAsEmployeeListTest.java
@@ -18,21 +18,41 @@ class PathAsEmployeeListTest {
         );
     }
 
-    @Test
-    void contains() {
-        MatcherAssert.assertThat(
-                "this employee is missing",
-                new PathAsEmployeeList("employee_test.xml").contains(
-                        new DefaultEmployee(new XMLDocument(
-                                """
-                                        <employee>
-                                            <name>George Smith</name>
-                                            <department>finance</department>
-                                        </employee>"""
-                        ))
-                ),
-                Matchers.is(true)
-        );
+    @Nested
+    class Contains {
+        @Test
+        void containsIsTrue() {
+            MatcherAssert.assertThat(
+                    "this employee is missing",
+                    new PathAsEmployeeList("employee_test_short.xml").contains(
+                            new DefaultEmployee(new XMLDocument(
+                                    """
+                                            <employee>
+                                                <name>George Smith</name>
+                                                <department>finance</department>
+                                            </employee>"""
+                            ))
+                    ),
+                    Matchers.is(true)
+            );
+        }
+
+        @Test
+        void containsIsTrueWhen() {
+            MatcherAssert.assertThat(
+                    "this employee is missing",
+                    new PathAsEmployeeList("employee_test.xml").contains(
+                            new DefaultEmployee(new XMLDocument(
+                                    """
+                                            <employee>
+                                                <name>George Smith</name>
+                                                <department>finance</department>
+                                            </employee>"""
+                            ))
+                    ),
+                    Matchers.is(true)
+            );
+        }
     }
 
     @Test
@@ -105,5 +125,42 @@ class PathAsEmployeeListTest {
                 ),
                 Matchers.is(true)
         );
+    }
+
+    @Test
+    void debugDetailedEquality() {
+        PathAsEmployeeList list = new PathAsEmployeeList("employee_test_short.xml");
+        DefaultEmployee searchEmployee = new DefaultEmployee(new XMLDocument(
+                """
+                <employee>
+                    <name>George Smith</name>
+                    <department>finance</department>
+                </employee>"""
+        ));
+
+        Employee firstFromList = list.get(0);
+
+        // Check names
+        String searchName = searchEmployee.name();
+        String listName = firstFromList.name();
+        System.out.println("Search name: '" + searchName + "' (length: " + searchName.length() + ")");
+        System.out.println("List name: '" + listName + "' (length: " + listName.length() + ")");
+        System.out.println("Names equal: " + searchName.equals(listName));
+
+        // Check departments
+        List<String> searchDepts = searchEmployee.departments();
+        List<String> listDepts = firstFromList.departments();
+        System.out.println("Search departments: " + searchDepts);
+        System.out.println("List departments: " + listDepts);
+        System.out.println("Departments equal: " + searchDepts.equals(listDepts));
+
+        // Check the Name objects themselves
+        if (firstFromList instanceof DefaultEmployee && searchEmployee instanceof DefaultEmployee) {
+            DefaultEmployee listEmp = (DefaultEmployee) firstFromList;
+            System.out.println("Name objects equal: " + searchEmployee.name.equals(listEmp.name));
+            System.out.println("Department objects equal: " + searchEmployee.departments.equals(listEmp.departments));
+        }
+
+        System.out.println("Final equals: " + searchEmployee.equals(firstFromList));
     }
 }
